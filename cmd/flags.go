35af6b8affbcd575740aad06d9c3165c0cfaaeaf
gdanko <gdanko@protonmail.com>
2025-02-14 11:27:01 -0800

    Change some text

--- cmd/flags.go
+++ cmd/flags.go
@@ -14,12 +14,12 @@ func GetPersistentFlags(cmd *cobra.Command, colorSupport bool, colorCount int) {
 	cmd.PersistentFlags().IntVarP(&flagLevel, "level", "l", 0, "print tree to <depth> level deep")
 	cmd.PersistentFlags().StringVarP(&flagUsername, "user", "u", "", "show only branches containing processes of <user>; cannot be used with --exclude-root")
 	cmd.PersistentFlags().BoolVarP(&flagCpu, "cpu", "c", false, "show CPU utilization percentage with each process, e.g., (c: 0.00%)")
-	cmd.PersistentFlags().BoolVarP(&flagThreads, "threads", "t", false, "show the thread count with each process, e.g., (t: xx)")
+	cmd.PersistentFlags().BoolVarP(&flagThreads, "threads", "t", false, "show the number of threads with each process, e.g., (t: xx)")
 	cmd.PersistentFlags().BoolVarP(&flagMemory, "memory", "m", false, "show the memory usage with each process, e.g., (m: x.y MiB)")
 	cmd.PersistentFlags().BoolVarP(&flagExcludeRoot, "exclude-root", "U", false, "don't show branches containing only root processes; cannot be used with --user")
 	cmd.PersistentFlags().StringVarP(&flagContains, "contains", "s", "", "show only branches containing process with <string> in commandline")
 	cmd.PersistentFlags().Int32VarP(&flagPid, "pid", "p", 0, "show only branches containing process <pid>")
-	cmd.PersistentFlags().BoolVarP(&flagNoPids, "no-pids", "", false, "do not show PIDs")
+	cmd.PersistentFlags().BoolVarP(&flagNoPids, "no-pids", "n", false, "do not show PIDs")
 	cmd.PersistentFlags().BoolVarP(&flagWide, "wide", "w", false, "wide output, not truncated to window width")
 	if colorSupport {
 		if colorCount >= 8 && colorCount < 256 {
