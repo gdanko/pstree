19006a4625abfa566e027496273e9a15835b8ffd
gdanko <gdanko@protonmail.com>
2025-02-16 11:07:13 -0800

    Update the usage string; bump the version; split the graphics modes; add -g/--pgid

--- cmd/root.go
+++ cmd/root.go
@@ -27,15 +27,19 @@ var (
 	flagCpu               bool
 	flagExcludeRoot       bool
 	flagGraphicsMode      int
+	flagIBM850            bool
 	flagLevel             int
 	flagMemory            bool
-	flagNoPids            bool
 	flagPid               int32
 	flagRainbow           bool
 	flagShowAll           bool
+	flagShowPgids         bool
+	flagShowPids          bool
 	flagThreads           bool
 	flagUsername          string
+	flagUTF8              bool
 	flagVersion           bool
+	flagVT100             bool
 	flagWide              bool
 	installedMemory       *mem.VirtualMemoryStat
 	processes             []pstree.Process
@@ -45,7 +49,7 @@ var (
 	usageTemplate         string
 	validAttributes       []string = []string{"age", "cpu", "mem"}
 	validAttributesString string   = strings.Join(validAttributes, ", ")
-	version               string   = "0.5.3"
+	version               string   = "0.5.4"
 	versionString         string
 	rootCmd               = &cobra.Command{
 		Use:    "pstree",
@@ -65,20 +69,20 @@ func init() {
 	if colorSupport {
 		colorizeString = " [--colorize]"
 		rainbowString = " [--rainbow]"
-		colorString = "[-C, --color-attr <str>]"
+		colorString = "[-C, --color <attr>]"
 
 	}
 	usageTemplate = fmt.Sprintf(
-		`Usage: pstree [-acUmntw] [-all]%s [-s, --contains <str>] [-l, --level <int>]
-	      [-g, --mode <int>] [-p, --pid <int>]%s [-u, --user <str>] 
-	      %s
+		`Usage: pstree [-acUimgptuvw] [-all]%s %s
+          [-s, --contains <pattern>] [-l, --level <level>]
+          [--pid <pid>]%s [--user <user>]
    or: pstree -V
 
 Display a tree of processes.
 
 {{.Flags.FlagUsages}}
 Process group leaders are marked with '='.
-`, colorizeString, rainbowString, colorString)
+`, colorString, colorizeString, rainbowString)
 
 	GetPersistentFlags(rootCmd, colorSupport, colorCount)
 	rootCmd.SetUsageTemplate(usageTemplate)
@@ -98,6 +102,10 @@ func pstreeRunCmd(cmd *cobra.Command, args []string) error {
 		return errors.New("only one of --color, --rainbow, and --color-attr can be used")
 	}
 
+	if (util.BtoI(flagIBM850) + util.BtoI(flagUTF8) + util.BtoI(flagVT100)) > 1 {
+		return errors.New("only one of --ibm-850, --utf-8, and --vt-100 can be used")
+	}
+
 	if flagColor != "" && !util.Contains(validAttributes, flagColor) {
 		errorMessage = fmt.Sprintf("valid options for --color-attr are: %s", validAttributesString)
 		return errors.New(errorMessage)
@@ -105,7 +113,7 @@ func pstreeRunCmd(cmd *cobra.Command, args []string) error {
 
 	if flagVersion {
 		versionString = fmt.Sprintf(`pstree %s
-Copyright (C) 2024 Gary Danko
+Copyright (C) 2025 Gary Danko
 
 pstree comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it under
@@ -146,6 +154,8 @@ For more information about these matters, see the files named COPYING.`,
 		flagArguments = true
 		flagCpu = true
 		flagMemory = true
+		flagShowPgids = true
+		flagShowPids = true
 		flagThreads = true
 	}
 
@@ -153,7 +163,7 @@ For more information about these matters, see the files named COPYING.`,
 		ColorAttr:       flagColor,
 		ColorizeOutput:  flagColorize,
 		GraphicsMode:    flagGraphicsMode,
-		HidePids:        flagNoPids,
+		IBM850Graphics:  flagIBM850,
 		InstalledMemory: installedMemory.Total,
 		MaxDepth:        flagLevel,
 		RainbowOutput:   flagRainbow,
@@ -161,6 +171,10 @@ For more information about these matters, see the files named COPYING.`,
 		ShowCpuPercent:  flagCpu,
 		ShowMemoryUsage: flagMemory,
 		ShowNumThreads:  flagThreads,
+		ShowPGIDs:       flagShowPgids,
+		ShowPIDs:        flagShowPids,
+		UTF8Graphics:    flagUTF8,
+		VT100Graphics:   flagVT100,
 		WideDisplay:     flagWide,
 	}
 	pstree.PrintTree(processes, startingPidIndex, "", screenWidth, currentLevel, displayOptions)
