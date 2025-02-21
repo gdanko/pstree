ffd7482f06577b8c06a3a52d6a6ba5f39abebe45
Gary Danko <gary.danko@servicenow.com>
2025-02-21 08:05:00 -0800

    Sort flags; add order-by flag

--- cmd/flags.go
+++ cmd/flags.go
@@ -10,30 +10,32 @@ import (
 )
 
 func GetPersistentFlags(cmd *cobra.Command, colorSupport bool, colorCount int, username string) {
+	cmd.PersistentFlags().BoolVarP(&flagAge, "age", "", false, "show the age of the process using the format (dd:hh:mm:ss)")
 	cmd.PersistentFlags().BoolVarP(&flagArguments, "arguments", "a", false, "show command line arguments")
+	cmd.PersistentFlags().StringVarP(&flagContains, "contains", "s", "", "show only branches containing processes with <pattern> in the command line")
+	cmd.PersistentFlags().BoolVarP(&flagCpu, "cpu", "c", false, "show CPU utilization percentage with each process, e.g., (c:0.00%)")
+	cmd.PersistentFlags().BoolVarP(&flagExcludeRoot, "exclude-root", "U", false, "don't show branches containing only root processes; cannot be used with --user")
 	cmd.PersistentFlags().BoolVarP(&flagIBM850, "ibm-850", "i", false, "use IBM-850 line drawing characters")
+	cmd.PersistentFlags().IntVarP(&flagLevel, "level", "l", 0, "print tree to <level> level deep")
+	cmd.PersistentFlags().BoolVarP(&flagMemory, "memory", "m", false, "show the memory usage with each process, e.g., (m:x.y MiB)")
+	cmd.PersistentFlags().BoolVarP(&flagNoPids, "no-pids", "", false, "do not show process IDs")
+	cmd.PersistentFlags().StringVarP(&flagOrderBy, "order-by", "o", "", fmt.Sprintf("sort the results by <field>; valid options are: %s", strings.Join(validOrderBy, ", ")))
+	cmd.PersistentFlags().BoolVarP(&flagShowPgids, "pgid", "g", false, "show process group IDs")
+	cmd.PersistentFlags().Int32VarP(&flagPid, "pid", "p", 0, "show only branches containing process <pid>")
+	cmd.PersistentFlags().BoolVarP(&flagThreads, "threads", "t", false, "show the number of threads with each process, e.g., (t:xx)")
+	cmd.PersistentFlags().StringSliceVarP(&flagUsername, "user", "", []string{}, "show only branches containing processes of <user>; this option can be used more than and cannot be used with --exclude-root")
 	cmd.PersistentFlags().BoolVarP(&flagUTF8, "utf-8", "u", false, "use UTF-8 (Unicode) line drawing characters")
 	cmd.PersistentFlags().BoolVarP(&flagVT100, "vt-100", "v", false, "use VT-100 line drawing characters")
 	cmd.PersistentFlags().BoolVarP(&flagShowAll, "all", "", false, "equivalent to -a --age -c -g -m -t")
-	cmd.PersistentFlags().IntVarP(&flagLevel, "level", "l", 0, "print tree to <level> level deep")
-	cmd.PersistentFlags().BoolVarP(&flagShowPgids, "pgid", "g", false, "show process group IDs")
-	cmd.PersistentFlags().StringSliceVarP(&flagUsername, "user", "", []string{}, "show only branches containing processes of <user>; this option can be used more than and cannot be used with --exclude-root")
-	cmd.PersistentFlags().BoolVarP(&flagCpu, "cpu", "c", false, "show CPU utilization percentage with each process, e.g., (c:0.00%)")
-	cmd.PersistentFlags().BoolVarP(&flagAge, "age", "", false, "show the age of the process using the format (dd:hh:mm:ss)")
-	cmd.PersistentFlags().BoolVarP(&flagThreads, "threads", "t", false, "show the number of threads with each process, e.g., (t:xx)")
-	cmd.PersistentFlags().BoolVarP(&flagMemory, "memory", "m", false, "show the memory usage with each process, e.g., (m:x.y MiB)")
-	cmd.PersistentFlags().BoolVarP(&flagExcludeRoot, "exclude-root", "U", false, "don't show branches containing only root processes; cannot be used with --user")
-	cmd.PersistentFlags().StringVarP(&flagContains, "contains", "s", "", "show only branches containing processes with <pattern> in the command line")
-	cmd.PersistentFlags().Int32VarP(&flagPid, "pid", "p", 0, "show only branches containing process <pid>")
-	cmd.PersistentFlags().BoolVarP(&flagNoPids, "no-pids", "", false, "do not show process IDs")
 	cmd.PersistentFlags().BoolVarP(&flagWide, "wide", "w", false, "wide output, not truncated to window width")
+
 	if colorSupport {
-		cmd.PersistentFlags().StringVarP(&flagColor, "color", "C", "", fmt.Sprintf("color the process name by given attribute; valid options are: %s;\ncannot be used with --color or --rainbow", strings.Join(validAttributes, ", ")))
+		cmd.PersistentFlags().StringVarP(&flagColor, "color", "C", "", fmt.Sprintf("color the process name by given attribute; valid options are: %s;\ncannot be used with --colorize or --rainbow", strings.Join(validAttributes, ", ")))
 		if colorCount >= 8 && colorCount < 256 {
-			cmd.PersistentFlags().BoolVarP(&flagColorize, "colorize", "", false, fmt.Sprintf("add some %s to the output; cannot be used with --color-attr or --rainbow", util.Color8()))
+			cmd.PersistentFlags().BoolVarP(&flagColorize, "colorize", "", false, fmt.Sprintf("add some %s to the output; cannot be used with --color or --rainbow", util.Color8()))
 		} else if colorCount >= 256 {
-			cmd.PersistentFlags().BoolVarP(&flagColorize, "colorize", "", false, gorainbow.Rainbow("add some beautiful color to the pstree output; cannot be used with --color-attr or --rainbow"))
-			cmd.PersistentFlags().BoolVarP(&flagRainbow, "rainbow", "", false, "please don't; cannot be used with --color or --color-attr")
+			cmd.PersistentFlags().BoolVarP(&flagColorize, "colorize", "", false, gorainbow.Rainbow("add some beautiful color to the pstree output; cannot be used with --color or --rainbow"))
+			cmd.PersistentFlags().BoolVarP(&flagRainbow, "rainbow", "", false, "please don't; cannot be used with --color or --colorize")
 		}
 	}
 	if username == "gdanko" || username == "gary.danko" {
