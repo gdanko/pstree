8a5c36374f44b255f1f2b07635bd947cd2b3abed
Gary Danko <gary.danko@servicenow.com>
2025-06-20 15:44:55 -0700

    Remove the return and exit here

--- pstree.go
+++ pstree.go
@@ -6,11 +6,12 @@ import (
 	"github.com/gdanko/pstree/cmd"
 )
 
+// main is the entry point for the pstree application.
+// It executes the root command and handles any errors that occur.
+// If an error is encountered, the program will exit with a non-zero status code.
 func main() {
 	err := cmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-
-	return
 }
