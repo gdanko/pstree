d8a2648f24644f996c662f728eff0aa6ead730c1
Gary Danko <gary.danko@servicenow.com>
2025-06-20 18:28:27 -0700

    Update flags to imply --compact-not

--- cmd/flags.go
+++ cmd/flags.go
@@ -46,7 +46,7 @@ func GetPersistentFlags(cmd *cobra.Command, colorSupport bool, colorCount int, u
 
 	// Color options
 	if colorSupport {
-		cmd.PersistentFlags().StringVarP(&flagColor, "color", "k", "", fmt.Sprintf("color the process name by given attribute; valid options are: %s;\ncannot be used with --colorize or --rainbow", strings.Join(validAttributes, ", ")))
+		cmd.PersistentFlags().StringVarP(&flagColor, "color", "k", "", fmt.Sprintf("color the process name by given attribute; implies --compact-not; valid options are: %s;\ncannot be used with --colorize or --rainbow", strings.Join(validAttributes, ", ")))
 		if colorCount >= 8 && colorCount < 256 {
 			cmd.PersistentFlags().BoolVarP(&flagColorize, "colorize", "C", false, fmt.Sprintf("add some %s to the output; cannot be used with --color or --rainbow", util.Color8()))
 		} else if colorCount >= 256 {
@@ -56,10 +56,10 @@ func GetPersistentFlags(cmd *cobra.Command, colorSupport bool, colorCount int, u
 	}
 
 	// Optional information
-	cmd.PersistentFlags().BoolVarP(&flagShowAll, "all", "A", false, "equivalent to -a -c -g -G -m -O -p -t -T; cannot be used with --uid-transitions or --user-transitions")
+	cmd.PersistentFlags().BoolVarP(&flagShowAll, "all", "A", false, "equivalent to -a -c -g -G -m -O -p -t -I; cannot be used with --uid-transitions or --user-transitions")
 	cmd.PersistentFlags().BoolVarP(&flagCompactNot, "compact-not", "n", false, "do not compact identical subtrees in output")
-	cmd.PersistentFlags().BoolVarP(&flagCpu, "cpu", "c", false, "show CPU utilization percentage with each process, e.g., (c:0.00%)")
-	cmd.PersistentFlags().BoolVarP(&flagMemory, "memory", "m", false, "show the memory usage with each process, e.g., (m:x.y MiB)")
+	cmd.PersistentFlags().BoolVarP(&flagCpu, "cpu", "c", false, "show CPU utilization percentage with each process, e.g., (c:0.00%); implies --compact-not")
+	cmd.PersistentFlags().BoolVarP(&flagMemory, "memory", "m", false, "show the memory usage with each process, e.g., (m:x.y MiB); implies --compact-not")
 	cmd.PersistentFlags().BoolVarP(&flagShowOwner, "show-owner", "O", false, "show the owner of the process")
 	cmd.PersistentFlags().BoolVarP(&flagShowPgids, "pgid", "g", false, "show process group IDs")
 	cmd.PersistentFlags().BoolVarP(&flagShowPids, "show-pids", "p", false, "show PIDs")
@@ -74,7 +74,7 @@ func GetPersistentFlags(cmd *cobra.Command, colorSupport bool, colorCount int, u
 	cmd.PersistentFlags().BoolVarP(&flagExcludeRoot, "exclude-root", "X", false, "don't show branches containing only root processes; cannot be used with --user")
 	cmd.PersistentFlags().Int32VarP(&flagPid, "pid", "P", 0, "show only branches containing process <pid>")
 	cmd.PersistentFlags().StringSliceVarP(&flagUsername, "user", "", []string{}, "show only branches containing processes of <user>; this option can be used more than and cannot be used with --exclude-root")
-	cmd.PersistentFlags().StringVarP(&flagContains, "contains", "s", "", "show only branches containing processes with <pattern> in the command line")
+	cmd.PersistentFlags().StringVarP(&flagContains, "contains", "s", "", "show only branches containing processes with <pattern> in the command line; implies --compact-not")
 	cmd.PersistentFlags().StringVarP(&flagOrderBy, "order-by", "o", "", fmt.Sprintf("sort the results by <field>; valid options are: %s", strings.Join(validOrderBy, ", ")))
 
 	// Debugging
