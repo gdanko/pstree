dcf68df0ff7a8492200a536590fe287e6bcc32da
Gary Danko <gary.danko@servicenow.com>
2025-07-05 17:06:32 -0700

    Add separate funcs for posix and windows

---
+++ pkg/pstree/metrics_windows.go
@@ -0,0 +1,18 @@
+//go:build windows
+// +build windows
+
+package pstree
+
+import (
+	"errors"
+
+	"github.com/shirou/gopsutil/v4/process"
+)
+
+type PGIDFunc func(proc *process.Process) (int, error)
+
+func getPGIDFunc() PGIDFunc {
+	return func(proc *process.Process) (int, error) {
+		return 0, errors.New("getpgid not supported on Windows")
+	}
+}
